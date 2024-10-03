ebe2dbbd40cb6e26531478ccee0630ecc8cfac05
nikolay5s <108671885+nikolay5s@users.noreply.github.com>
2024-10-03 10:10:25 +0300

    Make grpc gorutine not immediately crash

--- core_service/internal/server/grpc_server.go
+++ core_service/internal/server/grpc_server.go
@@ -57,7 +57,13 @@ func (s *server) newReaderGrpcServer() (func() error, *grpc.Server, error) {
 
 	go func() {
 		s.log.Infof("Reader gRPC server is listening on port: %s", s.cfg.GRPC.Port)
-		s.log.Fatal(grpcServer.Serve(l))
+		// start serving grpc
+		err := grpcServer.Serve(l)
+		s.log.Warn(err)
+		// wait 3 second, when grpc is ended to server. Is needed for log other fatals, if exists
+		time.Sleep(time.Second * 3)
+		// and crash error
+		s.log.Fatal(err)
 	}()
 
 	return l.Close, grpcServer, nil
